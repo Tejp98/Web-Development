bc653a548ec6a9b26616f4628fa8748ce222936c
Tej <tejmpatel98@gmail.com>
2020-04-17 20:54:37 +0530

    deployed using heroku

---
+++ Newsletter/index.js
@@ -0,0 +1,61 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+console.log("server running");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname+ "/index.html");
+
+
+});
+
+app.post("/", function(req, res) {
+  console.log(req.method);
+  var data = {
+    members: [{
+      email_address: req.body.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: req.body.fname,
+        LNAME: req.body.lname
+      }
+    }]
+  }
+
+
+  var options = {
+    url: "https://us4.api.mailchimp.com/3.0/lists/dccd2886cc",
+    method: "POST",
+    headers: {
+      "Authorization": "tejp98 c9ea4112dbb4b969c61f48eee58ed79f-us4"
+    },
+    body: JSON.stringify(data)
+  }
+  request(options, function(error, response, body) {
+    console.log(response.statusCode);
+    if (error) {
+      res.sendFile(__dirname+ "/failure.html");
+    } else {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname+ "/success.html")
+      } else {
+        res.sendFile(__dirname+ "/failure.html");
+      }
+    }
+  });
+
+});
+
+app.post("/failure", function(req, res){
+  res.sendFile(__dirname+ "/index.html");
+});
+app.listen(process.env.PORT || 3000);
